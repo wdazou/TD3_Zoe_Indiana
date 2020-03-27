5d59aa332168053cf7cf0a295128820b0e1ea934
wdazou <wdazou@localhost.localdomain>
2020-03-27 23:20:52 +0100

    message erreur()

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
-import sys 
+import sys
 
 def add(x,y):
 	return x + y
 
-x=int(sys.argv[1])
-y=int(sys.argv[2])
+if len(sys.argv) <= 2 :
+	print (" manque arguments")
 
-print (add(x,y))
+elif (len(sys.argv) == 3) :
+	x=int(sys.argv[1])
+	y=int(sys.argv[2])
+	print("Le resultat est : ",add(x,y))
+
+else :
+	print ("trop d'arguments")
+
