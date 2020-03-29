546218edfffe17f1b1b65e7fafc6137b38a26efe
Indiana1808 <jenitatindiana@yahoo.com>
2020-03-29 09:54:36 +0400

    0.3

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
-import sys
+def mul(a,b):
+        return a*b
 
-def mul(x,y):
-	return x * y
 
-if len(sys.argv) <= 2 :
-	print (" manque arguments")
 
-elif (len(sys.argv) == 3) :
-	x=int(sys.argv[1])
-	y=int(sys.argv[2])
-	print("Le resultat est : ",mul(x,y))
+def main():
+        import sys
+        entre = sys.argv
 
-else :
-	print ("trop d'arguments")
+
+        if (len(entre) != 3):
+                print("Erreur  inserer 2 valeurs s'il vous plait" )
+                a = input("entrez le premier valeur: ")
+                b = input("entrez le deuxieme valeur: ")
+
+                try: #il arrive que input ne retourne pas une  valeur "string" mais "int" (ex: kali linux)
+                        print(mul(eval(a),eval(b)))
+
+
+                except:
+                        print(mul(a,b))
+                return
+
+
+        print(mul(eval(entre[1]),eval(entre[2])))
+
+
+if (__name__ == "__main__"):
+        main()
 
